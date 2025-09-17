c1c38c787153d836df7f114a1cc4527a65aa0d2c
girinihaajit25-droid <girinihaaj.it25@bitsathy.ac.in>
2025-09-17 21:30:57 +0530

    Add files via upload

---
+++ ds task 2.py
@@ -0,0 +1,9 @@
+x=int(input("enter the cost price:"))
+y=int(input("enter the selling price:"))
+if x>y:
+      print("loss")
+elif x<y:
+    print("profit")
+else:
+    print("neither loss nor profit")
+    
