96acd4dd29cc22b29bea789e8966dad1ed3e77e5
girinihaajit25-droid <girinihaaj.it25@bitsathy.ac.in>
2025-09-17 21:31:58 +0530

    Delete ds task 2.py

--- ds task 2.py
+++
@@ -1,9 +0,0 @@
-x=int(input("enter the cost price:"))
-y=int(input("enter the selling price:"))
-if x>y:
-      print("loss")
-elif x<y:
-    print("profit")
-else:
-    print("neither loss nor profit")
-    
